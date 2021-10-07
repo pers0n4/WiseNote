d012513f6f7051b08dab72999a96c1db0455edaa
Dong-Young Kim <31337.persona@gmail.com>
2021-10-08 02:34:31 +0900

    feat: update sqlite db filename

--- server/database/session.py
+++ server/database/session.py
@@ -4,7 +4,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 # `check_same_thread` needed fot sqlite
-engine = create_engine("sqlite://", connect_args={"check_same_thread": False})
+engine = create_engine(
+    "sqlite:///./db.sqlite3", connect_args={"check_same_thread": False}
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
