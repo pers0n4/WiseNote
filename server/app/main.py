35af08cd166601fe5cd4090b10912dfbb09f053f
Dong-Young Kim <31337.persona@gmail.com>
2021-10-08 03:49:23 +0900

    feat: update main script

--- server/app/main.py
+++ server/app/main.py
@@ -1,14 +1,20 @@
+from typing import Any
+
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 
-from database import Base
+from app.api import router
+from database.base import Base
 from database.session import engine
 
-Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)  # type: ignore
 
 app = FastAPI()
 
 
 @app.get("/", response_class=RedirectResponse)
-def root():
+def root() -> Any:
     return "/docs"
+
+
+app.include_router(router)
