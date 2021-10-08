88e3cdc345ec5b098f1364a6f63b06d3d618f7ed
Dong-Young Kim <31337.persona@gmail.com>
2021-10-08 18:48:36 +0900

    feat: add notebook api

--- server/app/api/__init__.py
+++ server/app/api/__init__.py
@@ -1,9 +1,10 @@
 from fastapi.routing import APIRouter
 
-from . import auth, note, user
+from . import auth, note, notebook, user
 
 router = APIRouter()
 
 router.include_router(user.router, prefix="/users", tags=["user"])
 router.include_router(auth.router, prefix="/auth", tags=["auth"])
 router.include_router(note.router, prefix="/notes", tags=["note"])
+router.include_router(notebook.router, prefix="/notebooks", tags=["notebook"])
