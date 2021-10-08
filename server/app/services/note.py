4559c42c7ad09a746e47e603e02f61d0aa85e148
Dong-Young Kim <31337.persona@gmail.com>
2021-10-08 18:48:06 +0900

    feat: update note service for notebook

--- server/app/services/note.py
+++ server/app/services/note.py
@@ -1,18 +1,40 @@
-from typing import List
+from typing import Any, List
 from uuid import UUID
 
 from sqlalchemy.orm import Session
 
+from .. import services
 from ..models import Note
-from ..schemas import NoteCreate, NoteUpdate
+from ..schemas import NotebookCreate, NoteCreate, NoteInDBBase, NoteUpdate
 from .base import Service
 
 
 class NoteService(Service[Note, NoteCreate, NoteUpdate]):
     def create_with_user(
         self, db: Session, *, object_in: NoteCreate, user_id: UUID
-    ) -> Note:
-        note = Note(**object_in.dict(), user_id=user_id)  # type: ignore
+    ) -> Any:
+        note_data = object_in.dict(exclude_unset=True)
+
+        notebook = services.notebook.find_by_user_and_name(
+            db, user_id=user_id, name=note_data["notebook"]
+        )
+        if notebook is None:
+            print("Notebook not found")
+            notebook = services.notebook.create_with_user(
+                db,
+                object_in=NotebookCreate(name=note_data["notebook"]),
+                user_id=user_id,
+            )
+        print(notebook.name, notebook.id)
+        print(note_data)
+
+        note = Note(
+            **NoteInDBBase(
+                **note_data,
+                user_id=user_id,
+                notebook_id=notebook.id,
+            ).dict(exclude_unset=True)
+        )  # type: ignore
         db.add(note)
         db.commit()
         db.refresh(note)
