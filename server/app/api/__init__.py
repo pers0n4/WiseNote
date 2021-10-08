ed58b1cea2c8eb3c9f6b40362efef23f86637b25
Dong-Young Kim <31337.persona@gmail.com>
2021-10-08 17:09:22 +0900

    feat: add note api

--- server/app/api/__init__.py
+++ server/app/api/__init__.py
@@ -1,8 +1,9 @@
 from fastapi.routing import APIRouter
 
-from . import auth, user
+from . import auth, note, user
 
 router = APIRouter()
 
 router.include_router(user.router, prefix="/users", tags=["user"])
 router.include_router(auth.router, prefix="/auth", tags=["auth"])
+router.include_router(note.router, prefix="/notes", tags=["note"])
